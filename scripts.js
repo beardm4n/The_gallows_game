aa4ea1209f4492e29f022bde80e9c5dc340f4cf1
beardm4n <dmitriykorovko@gmail.com>
2019-04-29 13:08:30 +0300

    game creation

---
+++ scripts.js
@@ -0,0 +1,39 @@
+// choose the random word
+let words = [
+   "вышка", "порок", "булыжник", "посол", "заезд", "шарж", "шина", "морс", "озноб", "сахар", "конец", "пресс", "танец", "ось", "буря", "щеголь",
+   "антракт", "лидер", "носорог", "трость", "берег", "пласт", "золото", "ножка", "спутник", "ручей", "опрос", "карлик", "изгой", "разум", "кирка", "репей",
+   "повар", "кулич", "оранжерея", "храм", "нож", "отзыв", "клык", "память"
+];
+
+let word = words[Math.floor(Math.random() * words.length)].toLowerCase();
+
+// create a final array
+let finalArray = [];
+for (let i = 0; i < word.length; i++) {
+   finalArray[i] = "_";
+}
+
+let remainingLetters = word.length;
+
+// game cycle
+while (remainingLetters > 0) {
+   // display game status
+   alert(finalArray.join(" "));
+
+   // check entered answer
+   let guess = prompt("Угадайте букву или нажмите 'Отмена' для выхода");
+   if (guess === null) {
+      break;
+   } else if (guess.length !== 1) {
+      alert("Введите только одну букву");
+   } else {
+      // refresh game status
+      for (let j = 0; j < word.length; j++) {
+         if (word[j] === guess) {
+            finalArray[j] = guess;
+            remainingLetters--;
+         }
+      }
+   }
+}
+alert("Было загаданно слово - " + word);
