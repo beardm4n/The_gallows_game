729e543714d05dc230815b698a01a68c4a6fe8de
beardm4n <dmitriykorovko@gmail.com>
2019-06-02 13:39:59 +0300

    click on the title - game

--- Interactive programming/scripts.js
+++ Interactive programming/scripts.js
@@ -1,5 +1,7 @@
 let direction = "right",
-    offsetDirection = 0;
+    offsetDirection = 0,
+    intervalLength = 25,
+    clicks = 0;
 
 // point of origin
 $("#heading").offset({
@@ -12,22 +14,22 @@ let moveHeading = function () {
    if (direction === "right") {
       $("#heading").offset({left: offsetDirection});
       offsetDirection++;
-      if (offsetDirection > 200) {
+      if (offsetDirection > 75) {
          offsetDirection = 0;
          direction = "down";
       }
    } else if (direction === "down") {
       $("#heading").offset({top: offsetDirection});
       offsetDirection++;
-      if (offsetDirection > 200) {
-         offsetDirection = 200;
+      if (offsetDirection > 75) {
+         offsetDirection = 75;
          direction = "left";
       }
    } else if (direction === "left") {
       $("#heading").offset({left: offsetDirection});
       offsetDirection--;
       if (offsetDirection < 0) {
-         offsetDirection = 200;
+         offsetDirection = 75;
          direction = "up";
       }
    } else if (direction === "up") {
@@ -40,4 +42,17 @@ let moveHeading = function () {
    }
 };
 
-setInterval(moveHeading, 10);
+let intervalId = setInterval(moveHeading, intervalLength);
+
+// click on the title
+$("#heading").click(function () {
+   clearInterval(intervalId);
+   intervalLength /= 2.5;
+   clicks++;
+
+   if (clicks > 4) {
+      $("#heading").text("YOU WON! ヽ(・∀・)ﾉ")
+   } else {
+      intervalId = setInterval(moveHeading, intervalLength);
+   }
+});
