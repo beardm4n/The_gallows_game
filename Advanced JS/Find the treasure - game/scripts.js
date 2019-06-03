c76cb3fb184a89e968429950e5a82407d72078f8
beardm4n <dmitriykorovko@gmail.com>
2019-06-03 18:01:41 +0300

    the penultimate version of the game

--- Advanced JS/Find the treasure - game/scripts.js
+++ Advanced JS/Find the treasure - game/scripts.js
@@ -1,29 +1,62 @@
-// counter
-let clicks = 0;
-
 // we need to get a random number for treasure
 let getRandomNumber = function(size) {
-  return Math.random(Math.floor() * size);
+  return Math.floor(Math.random() * size);
 };
 
-// set treasure's coordinates
-let width = 400,
+// click to treasure distance
+let getDistance = function (event, target) {
+   let diffX = event.offsetX - target.x,
+       diffY = event.offsetY - target.y;
+
+   return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+// tell the player how close he is to the target
+let getDistanceHint = function (distance) {
+   if (distance < 10) {
+      return "Get burned"
+   } else if (distance < 20) {
+      return "Very hot"
+   } else if (distance < 40) {
+      return "Hot"
+   } else if (distance < 80) {
+      return "Heat"
+   } else if (distance < 160) {
+      return "Coldly"
+   } else if (distance < 320) {
+      return "Very cold"
+   }
+};
+
+let clicks = 0,
+    width = 400,
     height = 400;
 
+// set treasure's random coordinates
 let target = {
-  x: getRandomNumber(width),
-  y: getRandomNumber(height)
+   x: getRandomNumber(width),
+   y: getRandomNumber(height)
 };
+console.log(target.x);
+console.log(target.y);
 
 // event handler
 $("#map").click(function (event) {
    clicks++;
 
-// click to treasure distance
-   let getDistance = function (event, target) {
-      let diffX = event.offsetX - target.x,
-          diffY = event.offsetY - target.y;
+   // we get the distance from the click to the treasure
+   let distance = getDistance(event, target);
 
-      return Math.sqrt((diffX * diffX) + (diffY * diffY));
+   // convert distance to hint
+   let distanceHint = getDistanceHint(distance);
+
+   // write a new hint to the #distance
+   $("#distance").text(distanceHint);
+
+   // win check
+   if (distance < 8) {
+      $("#win").text("Treasure found! You made " + clicks + " clicks");
+      // alert("Treasure found! You made " + clicks + " clicks");
    }
 });
+
