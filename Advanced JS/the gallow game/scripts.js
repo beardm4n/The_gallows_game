f26bdff3e826f3d8087734cb79d8778b1f8d202a
beardm4n <dmitriykorovko@gmail.com>
2019-06-19 15:47:39 +0300

    updated game code

--- Advanced JS/the gallow game/scripts.js
+++ Advanced JS/the gallow game/scripts.js
@@ -1,49 +1,111 @@
-// choose the random word
-let words = [
-   "вышка", "порок", "булыжник", "посол", "заезд", "шарж", "шина", "морс", "озноб", "сахар", "конец", "пресс", "танец", "ось", "буря", "щеголь",
-   "антракт", "лидер", "носорог", "трость", "берег", "пласт", "золото", "ножка", "спутник", "ручей", "опрос", "карлик", "изгой", "разум", "кирка", "репей",
-   "повар", "кулич", "оранжерея", "храм", "нож", "отзыв", "клык", "память"
-];
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
 
-let word = words[Math.floor(Math.random() * words.length)].toLowerCase();
+// choose the random word
+let pickWord = function() {
+   let words = [
+      "вышка", "порок", "булыжник", "посол", "заезд", "шарж", "шина", "морс", "озноб", "сахар", "конец", "пресс", "танец", "ось", "буря", "щеголь",
+      "антракт", "лидер", "носорог", "трость", "берег", "пласт", "золото", "ножка", "спутник", "ручей", "опрос", "карлик", "изгой", "разум", "кирка", "репей",
+      "повар", "кулич", "оранжерея", "храм", "нож", "отзыв", "клык", "память"
+   ];
+
+   return words[Math.floor(Math.random() * words.length)].toLowerCase();
+};
 
 // create a final array
-let finalArray = [];
+let setupAnswerArray = function(word){
+   let answerArray = [];
+   for (let i = 0; i < word.length; i++) {
+      answerArray[i] = "_";
+   }
 
-for (let i = 0; i < word.length; i++) {
-   finalArray[i] = "_";
-}
+   return answerArray;
+};
 
-let remainingLetters = word.length,
-    count = 0;
+// game progress
+let showPlayerProgress = function(answerArray) {
+  alert(answerArray.join(" "));
+};
 
-// game cycle
-while (remainingLetters > 0 && count !== 11) {
-   // display game status
-   alert(finalArray.join(" "));
+// game condition
+let getGuess = function() {
+  return prompt("Угадайте букву или нажмите 'Отмена' для выхода из игры");
+};
 
-   // check entered answer
-   let guess = prompt("Угадайте букву или нажмите 'Отмена' для выхода").toLowerCase();
+// game status
+let updateGameState = function(guess, word, answerArray) {
+   let count = 0;
+   for (let j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+         answerArray[j] = guess;
+         count++;
+      }
+   }
+   return count;
+};
+
+let showAnswerAndCongratulatePlayer = function (answerArray) {
+  showPlayerProgress(answerArray);
+  alert("Отлично! Была загадано слово " + answerArray.join(""));
+};
+
+//drawing
+let drawSegment = function (incorrectGuesses) {
+  ctx.lineWidth = 4;
+
+  if (incorrectGuesses === 0) {
+     ctx.strokeRect(20, 20, 20, 20);
+  } else if (incorrectGuesses === 1) {
+     ctx.beginPath();
+     ctx.moveTo(30, 40);
+     ctx.lineTo(30, 80);
+     ctx.stroke();
+  } else if (incorrectGuesses === 2) {
+     ctx.beginPath();
+     ctx.moveTo(30, 80);
+     ctx.lineTo(10, 110);
+     ctx.stroke();
+  } else if (incorrectGuesses === 3) {
+     ctx.beginPath();
+     ctx.moveTo(30, 80);
+     ctx.lineTo(50, 110);
+     ctx.stroke();
+  } else if (incorrectGuesses === 4) {
+     ctx.beginPath();
+     ctx.moveTo(30, 60);
+     ctx.lineTo(10, 50);
+     ctx.stroke();
+  } else if (incorrectGuesses === 5) {
+     ctx.beginPath();
+     ctx.moveTo(30, 60);
+     ctx.lineTo(50, 50);
+     ctx.stroke();
+  }
+};
+
+let word = pickWord();
+let answerArray = setupAnswerArray(word);
+let remainingLetters = word.length;
+let incorrectGuesses = 0;
+
+while  (remainingLetters > 0) {
+   showPlayerProgress(answerArray);
+   let guess = getGuess();
 
    if (guess === null) {
       break;
    } else if (guess.length !== 1) {
-      alert("Введите только одну букву");
+      alert("Пожалуйста, введите одиночную букву.");
    } else {
-      // refresh game status
-      for (let j = 0; j < word.length; j++) {
+      let correctGuesses = updateGameState(guess, word, answerArray);
 
-         if (word[j] === guess && finalArray[j] === "_") {
-            finalArray[j] = guess;
-            remainingLetters--;
-         }
+      remainingLetters -= correctGuesses;
+
+      if (correctGuesses === 0) {
+         drawSegment(incorrectGuesses);
+         incorrectGuesses++
       }
    }
-   count++;
 }
 
-if (count === 11) {
-   alert("Вы проиграли =(. Загаданное слово " + word);
-} else {
-   alert("Вы угадали! Это слово - " + word);
-}
+showAnswerAndCongratulatePlayer(answerArray);
