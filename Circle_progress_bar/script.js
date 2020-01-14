8b6c40bcf49aa0db4f921e03a5ffc0bf3b2b34e6
beardm4n <dmitriykorovko@gmail.com>
2020-01-14 18:16:52 +0300

    added some function for animation

--- Circle_progress_bar/script.js
+++ Circle_progress_bar/script.js
@@ -1,18 +1,66 @@
-const circle = document.querySelector('.progress-ring__circle');
-const radius = circle.r.baseVal.value;
+const day = document.querySelector('.day');
+const hour = document.querySelector('.progress-ring__hours');
+const min = document.querySelector('.progress-ring__minutes');
+const sec = document.querySelector('.progress-ring__seconds');
+const radiusH = hour.r.baseVal.value;
+const radiusM = min.r.baseVal.value;
+const radiusS = sec.r.baseVal.value;
 // calculate the circumference
-const circumference = 2 * Math.PI * radius;
-const input = document.querySelector('.percent');
+const circumferenceH = 2 * Math.PI * radiusH;
+const circumferenceM = 2 * Math.PI * radiusM;
+const circumference = 2 * Math.PI * radiusS;
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = circumference;
+hour.style.strokeDasharray = `${circumferenceH} ${circumferenceH}`;
+hour.style.strokeDashoffset = circumferenceH;
 
-input.addEventListener('change', () => {
-    setProgress(input.value);
-})
+min.style.strokeDasharray = `${circumferenceM} ${circumferenceM}`;
+min.style.strokeDashoffset = circumferenceM;
 
-function setProgress(percent) {
-    const offset = circumference - percent / 100 * circumference;
+sec.style.strokeDasharray = `${circumference} ${circumference}`;
+sec.style.strokeDashoffset = circumference;
 
-    circle.style.strokeDashoffset = offset;
+function setHours(hours) {
+    const offset = circumferenceH - hours / 12 * circumferenceH;
+    hour.style.strokeDashoffset = offset;
 }
+
+function setMinutes(minutes) {
+    const offset = circumferenceM - minutes / 60 * circumferenceM;
+    min.style.strokeDashoffset = offset;
+}
+
+function setSeconds(seconds) {
+    const offset = circumference - seconds / 60 * circumference;
+    sec.style.strokeDashoffset = offset;
+}
+
+function getHours() {
+    const time = new Date(),
+          h = time.getHours();
+
+    (h > 0 && h < 12) ? day.textContent = 'AM' : day.textContent = 'PM'; 
+
+    if (h > 0 && h < 12) {
+        setHours(h);
+    } else {
+        setHours(h - 12);
+    }
+}
+
+setInterval(getHours, 1000)
+
+function getMinutes() {
+    const time = new Date(),
+          m = time.getMinutes() + 1;
+    setMinutes(m);
+}
+
+setInterval(getMinutes, 1000)
+
+function getSeconds() {
+    const time = new Date();
+    let s = time.getSeconds() + 1;
+    setSeconds(s);
+}
+
+setInterval(getSeconds, 500);
