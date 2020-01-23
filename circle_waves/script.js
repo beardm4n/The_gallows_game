92957a4a9263ac9010d499f5c2af07b1beab797a
beardm4n <dmitriykorovko@gmail.com>
2020-01-23 22:56:47 +0300

    some script added

--- circle_waves/script.js
+++ circle_waves/script.js
@@ -15,13 +15,16 @@
    // параметры отображения волны и окружности:
    function updateRing() {
       for (let i = 0; i < numberOfRings; i++) {
-         drawRing()
+         drawRing();
       }
    }
 
    // перенесем отрисовку в центр экрана:
    let centerX = canvas.width / 2;
    let centerY = canvas.height / 2;
+   const maxWaveAmplitude = 17;
+   const numOfWaves = 7;
+   const ringRadius = 150;
 
    // отрисовка окружности:
    function drawRing() {
@@ -32,14 +35,29 @@
       
       // нарисуем круг: с каждой итерацией будем менять угол 
       // и рисовать линию по окружности
-      for (let i = 0; i < 360; i++) {
+      for (let i = -180; i < 180; i++) {
          // переводим градусы в радианы, чтобы передать их cos и sin, 
          // т.к. они принимают радианы в качестве аргументов
          let currentAngle = i * Math.PI / 180; 
-         let x = centerX + Math.cos(currentAngle) * 100;
-         let y = centerY + Math.sin(currentAngle) * 100;
+
+         // эффект перехода из окружности в волну и обратно в окружность
+         let displacement = 0;
+
+         let now = Math.abs(i);
+
+         if (i > 70) {
+            displacement = (i - 70) / 70;
+         }
+         
+         if (displacement >= 1) displacement = 1;
+
+         // создание волны. для этого исп. метод синуса
+         let waveAmplitude = ringRadius + displacement * Math.sin(currentAngle * numOfWaves) * maxWaveAmplitude;
+
+         let x = centerX + Math.cos(currentAngle) * waveAmplitude;
+         let y = centerY + Math.sin(currentAngle) * waveAmplitude;
          // добавляем 100 к centerX, чтобы убрать прямую линию, исходящую из центра окружности
-         ( i > 0 ) ? ctx.lineTo(x, y) : ctx.moveTo(centerX + 100, centerY);
+         ( i > -180 ) ? ctx.lineTo(x, y) : ctx.moveTo(centerX + ringRadius, centerY);
       }
 
       ctx.closePath();
