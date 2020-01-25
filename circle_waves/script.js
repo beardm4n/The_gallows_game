03419e364a5b2ae5af7aa15a293dd0df23b75d00
beardm4n <dmitriykorovko@gmail.com>
2020-01-25 11:23:52 +0300

    ring animation added

--- circle_waves/script.js
+++ circle_waves/script.js
@@ -3,7 +3,16 @@
    const canvas = document.querySelector('canvas');
    const ctx = canvas.getContext('2d');
    // const numberOfRings = prompt('How many rings to draw?', 0);
-   const numberOfRings = 1;
+   const numberOfRings = 3;
+   // радиус окружности
+   const ringRadius = 150;
+
+   // параметр, увеличивающий радиус каждого нового кольца
+   const ringRadiusOffset = 7;
+
+   // счетчик для начального угла отрисовки окружности
+   let startAngle = 0;
+
    // Размер canvas зададим через функцию:
    function init() {
       canvas.width = innerWidth;
@@ -15,8 +24,11 @@
    // параметры отображения волны и окружности:
    function updateRing() {
       for (let i = 0; i < numberOfRings; i++) {
-         drawRing();
+         let radius = i * ringRadiusOffset + ringRadius;
+         drawRing(radius);
       }
+      
+      (startAngle >= 360) ? startAngle = 0 : startAngle++;
    }
 
    // перенесем отрисовку в центр экрана:
@@ -24,10 +36,9 @@
    let centerY = canvas.height / 2;
    const maxWaveAmplitude = 17;
    const numOfWaves = 7;
-   const ringRadius = 150;
 
    // отрисовка окружности:
-   function drawRing() {
+   function drawRing(radius) {
       ctx.strokeStyle = 'red';
       ctx.lineWidth = 5;
 
@@ -38,7 +49,7 @@
       for (let i = -180; i < 180; i++) {
          // переводим градусы в радианы, чтобы передать их cos и sin, 
          // т.к. они принимают радианы в качестве аргументов
-         let currentAngle = i * Math.PI / 180; 
+         let currentAngle = (i + startAngle) * Math.PI / 180; 
 
          // эффект перехода из окружности в волну и обратно в окружность
          let displacement = 0;
@@ -52,12 +63,12 @@
          if (displacement >= 1) displacement = 1;
 
          // создание волны. для этого исп. метод синуса
-         let waveAmplitude = ringRadius + displacement * Math.sin(currentAngle * numOfWaves) * maxWaveAmplitude;
+         let waveAmplitude = radius + displacement * Math.sin(currentAngle * numOfWaves) * maxWaveAmplitude;
 
          let x = centerX + Math.cos(currentAngle) * waveAmplitude;
          let y = centerY + Math.sin(currentAngle) * waveAmplitude;
          // добавляем 100 к centerX, чтобы убрать прямую линию, исходящую из центра окружности
-         ( i >= -180 ) ? ctx.lineTo(x, y) : ctx.moveTo(centerX + ringRadius, centerY);
+         ( i >= -180 ) ? ctx.lineTo(x, y) : ctx.moveTo(centerX + radius, centerY);
       }
 
       ctx.closePath();
