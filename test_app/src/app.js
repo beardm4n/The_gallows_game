1853d507a1e11a2672a90b7c128e44a967191a19
beardm4n <dmitriykorovko@gmail.com>
2020-01-31 22:25:04 +0300

    initial webpack setup

---
+++ test_app/src/app.js
@@ -0,0 +1,1 @@
+console.warn('My App working! =)');
--- test_app/webpack.config.js
+++ test_app/webpack.config.js
@@ -1,3 +1,16 @@
+const path = require('path'); // модуль позволит работать с путями в текущей ОС
+
 module.exports = {
-   
+   // входной файл приложения
+   entry: './src/app.js',
+   // здесь указывается во что в итоге webpack превратит написаный код.
+   output: {                
+      filename: 'bundle.js',
+      // устанавливаем путь к файлу куда будут собираться все соединенные JS-файлы
+      path: path.resolve(__dirname, 'build'),
+   },
+   // настройка devServer
+   devServer: {
+      port: 3000,
+   },
 }
