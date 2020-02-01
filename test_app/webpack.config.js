346c02b4c0ef2363fae0656ce9a8edf317493ee8
beardm4n <dmitriykorovko@gmail.com>
2020-02-01 14:56:21 +0300

    html plugin connected and instance of html plugin created

--- test_app/webpack.config.js
+++ test_app/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path'); // модуль NodeJs позволит работать с путями в текущей ОС
+const HTMLPlugin = require('html-webpack-plugin'); // подключаем пакет 
 
 module.exports = {
    // входной файл приложения
    entry: './src/app.js',
    // здесь указывается во что в итоге webpack превратит написаный код.
    output: {                
-      filename: 'bundle.js',
+      filename: 'bundle.js', 
       // устанавливаем путь к файлу куда будут собираться все соединенные JS-файлы
       path: path.resolve(__dirname, 'build'),
    },
@@ -13,4 +14,10 @@ module.exports = {
    devServer: {
       port: 3000,
    },
+   // подключаем плагины
+   plugin: [
+      new HTMLPlugin({
+         template: './src/index.html',
+      })
+   ],
 }
