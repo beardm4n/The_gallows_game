c4e0430a38731c943fc840ea3b697257f569b79f
beardm4n <dmitriykorovko@gmail.com>
2020-02-05 22:10:03 +0300

    added some script and created utils file

--- test_app/src/app.js
+++ test_app/src/app.js
@@ -1,3 +1,14 @@
 import './style.css';
 
-console.warn('My App working! =)');
+const form = document.getElementById('form');
+
+// для того, чтобы не делать запрос к input и button по всему document, обратимся к form
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+
+function submitFormHandler(event) {
+   event.preventDefault(); // отменяем поведение по умолчанию, в данном случае - обновление страницы
+   console.warn(input.value);
+}
